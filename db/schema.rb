e21fcf6f7947849f483d0ebda2c2c0ef320088a2
Aquiles Valdivia <mail@aquiles.de>
2018-05-21 18:07:33 +0200

    booking mvc

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_21_132627) do
+ActiveRecord::Schema.define(version: 2018_05_21_144219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.float "price"
+    t.string "service"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +44,5 @@ ActiveRecord::Schema.define(version: 2018_05_21_132627) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "users"
 end
