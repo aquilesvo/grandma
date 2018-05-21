53683f40b85d88f2df99f7b94491c5c4af432dc1
Raffael Kniep <leus_raff@yahoo.de>
2018-05-21 17:38:27 +0200

    starting to work on new branch

--- db/schema.rb
+++ db/schema.rb
@@ -46,4 +46,5 @@ ActiveRecord::Schema.define(version: 2018_05_21_133158) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
