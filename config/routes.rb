ed4693103ef67885ba01d7040a5101112f0d1014
Raffael Kniep <leus_raff@yahoo.de>
2018-05-22 13:55:19 +0200

    CRUD functions working. navbar-links installed. Need to continue work on model-dependencies.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :grandmoms do
-    resources :bookings, only: [:index, :show, :new, :create, :destroy]
+    resources :bookings, only: [:show, :new, :create, :destroy]
   end
+  resources :bookings, only: [:index]
 end
