fade781e170c20de0b0775c149dbf64c3d0accb4
Raffael Kniep <leus_raff@yahoo.de>
2018-05-25 16:31:08 +0200

    seed file further but not working with fotos yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,22 @@ require 'faker'
 User.destroy_all
 Grandmom.destroy_all
 
-images = (1..14).to_a.map do |n|
-  'images/seed-omas/g#{n}.jpg'
-end
+images = [
+  "http://res.cloudinary.com/twentythree/image/upload/v1527256890/g1.jpg",
+  "http://res.cloudinary.com/twentythree/image/upload/v1527255970/g2.jpg",
+  "http://res.cloudinary.com/twentythree/image/upload/v1527255945/g3.jpg",
+  "http://res.cloudinary.com/twentythree/image/upload/v1527253290/g4.jpg",
+  "http://res.cloudinary.com/twentythree/image/upload/v1527253274/g5.jpg",
+  "http://res.cloudinary.com/twentythree/image/upload/v1527253256/g6.jpg",
+  "http://res.cloudinary.com/twentythree/image/upload/v1527253197/g7.jpg",
+  "http://res.cloudinary.com/twentythree/image/upload/v1527251543/g8.jpg",
+  "http://res.cloudinary.com/twentythree/image/upload/v1527249487/g9.jpg",
+  "http://res.cloudinary.com/twentythree/image/upload/v1527249451/g10.jpg",
+  "http://res.cloudinary.com/twentythree/image/upload/v1527249413/g11.jpg",
+  "http://res.cloudinary.com/twentythree/image/upload/v1527247797/g12.jpg",
+  "http://res.cloudinary.com/twentythree/image/upload/v1527178421/g13.jpg",
+  "http://res.cloudinary.com/twentythree/image/upload/v1527166506/g14.jpg"
+]
 
 addresses = [" Alexanderpl. 1, 10178 Berlin ", "Revaler Str. 1, 10243 Berlin", "Rigaer Str. 94, 10247 Berlin", "Mariannen Str. 27, 10999 Berlin", "Jonas Str. 41, 14053", "Pfalzburger Str. 29, 10717 Berlin", "Sonnenallee 137, 12059 Berlin", "Storkower Str. 121, 10407 Berlin", "Am Wriezener Bahnhof, 10243 Berlin", "Schicklerstraße 4, 10179 Berlin", "Mariannen Str. 6, 10997 Berlin", "An der Apostelkirche 12, 10783 Berlin"]
 
@@ -21,7 +34,7 @@ addresses = [" Alexanderpl. 1, 10178 Berlin ", "Revaler Str. 1, 10243 Berlin", "
   user = User.new({
     email: Faker::Internet.email,
     password: Faker::Job.title,
-    photo: images.sample,
+    photo: "http://res.cloudinary.com/twentythree/image/upload/v1527256813/profile_placeholder.jpg"
   })
   user.save!
 end
